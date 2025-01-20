a01e06dfcf07a82b42aa1715da988c205846805a
jhjhjhlll88 <167087417+jhjhjhlll88@users.noreply.github.com>
2025-01-20 18:28:39 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -29,8 +29,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'huihui011114@gmail.com'
-EMAIL_HOST_PASSWORD = 'ycoa freu hmhq urza'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'API key'
 
 
 ALLOWED_HOSTS = ['fypfsktm.herokuapp.com', 'localhost']
